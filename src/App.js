3493dbada071ea69bd13e6e4b06fcaf27b39377d
Rostyslav <rklyba@yahoo.com>
2021-02-16 16:25:19 +0200

    Set functionality for editing item

--- src/App.js
+++ src/App.js
@@ -18,9 +18,20 @@ function App() {
       // display alert
       showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
-      // deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed')
     } else {
-      showAlert(true, 'success', 'item added to the list')
+      showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
@@ -31,18 +42,23 @@ function App() {
     setAlert({ show, type, msg });
   };
   const clearList = () => {
-    showAlert(true, 'danger', 'empty list');
+    showAlert(true, "danger", "empty list");
     setList([]);
-  }
+  };
   const removeItem = (id) => {
-    showAlert(true, 'danger', 'item removed');
-    setList(list.filter((item) => item.id !== id)) 
-  }
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
+  };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert = {showAlert} 
-        list={list} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>grocery list</h3>
         <div className="form-control">
           <input
@@ -59,8 +75,10 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} removeItem = {removeItem}/>
-          <button className="clear-btn" onClick={clearList}>clear items</button>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
